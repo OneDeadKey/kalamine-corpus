588c4a29e260ae3fa29d6bb15a1392c44b6096e9
Maiann88 <maiann29@proton.me>
2024-12-23 10:11:54 +0100

    1 trailing line, rm volume after cat

--- bin/extract.sh
+++ bin/extract.sh
@@ -13,13 +13,17 @@ done
 sed -i '/\[Illustration\]/d'  txt/*.txt
 sed -i '1,/START/d' txt/*.txt
 sed -i '/END OF THE PROJECT GUTENBERG/,$d' txt/*.txt
-sed -i '/End of Project/d' txt/*.txt
+sed -i '/^End of Project Gutenberg/d' txt/*.txt
 sed -i '/\*       \*/d' txt/*.txt 
 for f in txt/*-[1-9].txt
 do
-    tome=$(echo "$f" | sed 's/-[1-9].txt//g');
-    cat "$tome"-[1-9].txt > "$tome".txt
+    volume=$(echo "$f" | sed 's/-[1-9].txt//g');
+    cat "$volume"-[1-9].txt > "$volume".txt
 done
+if [ -f "$volume".txt ]
+then
+    rm "$volume"-[1-9].txt
+fi
 
 echo "Leipzig..."
 for corpus in corpus/leipzig/*.tar.gz
@@ -30,5 +34,4 @@ do
     tar -xzf $corpus "$name"/"$name"-sentences.txt -O \
         | awk '!($1="")' > txt/"$name".txt
 done
-
 
