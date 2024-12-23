e96fdb90d381159895858f2c9d81fea0d9d8b81f
Maiann88 <maiann29@proton.me>
2024-12-23 09:02:22 +0100

    Removing useless lines + concat volumes into books

--- bin/extract.sh
+++ bin/extract.sh
@@ -10,6 +10,16 @@ do
     echo "...  $name"
     cp $corpus txt/$name.txt
 done
+sed -i '/\[Illustration\]/d'  txt/*.txt
+sed -i '1,/START/d' txt/*.txt
+sed -i '/END OF THE PROJECT GUTENBERG/,$d' txt/*.txt
+sed -i '/End of Project/d' txt/*.txt
+sed -i '/\*       \*/d' txt/*.txt 
+for f in txt/*-[1-9].txt
+do
+    tome=$(echo "$f" | sed 's/-[1-9].txt//g');
+    cat "$tome"-[1-9].txt > "$tome".txt
+done
 
 echo "Leipzig..."
 for corpus in corpus/leipzig/*.tar.gz
@@ -20,3 +30,5 @@ do
     tar -xzf $corpus "$name"/"$name"-sentences.txt -O \
         | awk '!($1="")' > txt/"$name".txt
 done
+
+
